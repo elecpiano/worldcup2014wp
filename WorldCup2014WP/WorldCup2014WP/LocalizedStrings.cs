fd235e5a2bb03904c41277af66e22297ac5c8b2b
elecpiano <elecpiano@gmail.com>
2014-05-06 03:49:57 +0800

    initial project

---
+++ WorldCup2014WP/WorldCup2014WP/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using WorldCup2014WP.Resources;
+
+namespace WorldCup2014WP
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
